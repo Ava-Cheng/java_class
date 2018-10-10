90fdc29f0d6144911efdf6fa4c47bcbc54fcdabf
鄭育存 <love0638918@gmail.com>
2018-10-10 09:19:47 +0000

    Update HW03_04.java

--- C107118202_P03/src/c107118202_p03/HW03_04.java
+++ C107118202_P03/src/c107118202_p03/HW03_04.java
@@ -7,20 +7,23 @@ public class HW03_04 {
     public static void main(String[] args) {
         int num;
         int k = 2;
-        String str = "no";
+        String str = "yes";
         Scanner input = new Scanner(System.in);
         System.out.println("請輸入一個數，判斷該數是否為質數。");
         num = input.nextInt();
         while (num > k) {
-            if (num % k == 0) {
-                str = "yes";
+            if ((num % k == 0)) {
+                str = "no";
             }
             k++;
         }
-        if (str == "yes") {
-            System.out.printf("%d不是質數\n", num);
-        } else {
+        if(num<=1){
+             str = "no";
+        }
+        if ("yes".equals(str)) {
             System.out.printf("%d是質數\n", num);
+        } else {
+            System.out.printf("%d不是質數\n", num);
         }
 
     }
